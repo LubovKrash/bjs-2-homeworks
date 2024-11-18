e0378c37414b8209ce9af34e0844a0185fd953e4
LubovKrasheninnikova <altukhovalv@yandex.ru>
2024-11-18 10:22:22 +0300

    Третье задание готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.lenght === arr2.lenght && arr1.every(function(value, index) {
+        return value === arr2[index];
+    });  
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+    const filteredUsers = users.filter(function(user) {
+        return user.gender === gender;
+    }); // Фильтруем пользователей по полу
+
+    if (filteredUsers.lenght === 0) { 
+        return 0;
+    }  // Если нет, то возвращаем 0
+
+    const middleAge = filteredUsers.reduce(function(sum, user) {
+        return sum + user.age;
+    }, 0); // Считаем средний возраст
+
+    return middleAge / filteredUsers.lenght;
   
 }
